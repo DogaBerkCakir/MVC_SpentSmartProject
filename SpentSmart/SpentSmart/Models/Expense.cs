89bfa0a64ab37ea52aed14bac03a51b3deb51cff
Doğa Berk Çakır <107003857+DogaBerkCakir@users.noreply.github.com>
2025-03-16 14:33:26 +0300

    SpentSmartProject
    ASP.NET core MVC

---
+++ SpentSmart/SpentSmart/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpentSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "0 >")]
+        public decimal Value { get; set; }
+        [Required(ErrorMessage ="can not empty")]
+        public string? Description { get; set; }
+    }
+}
